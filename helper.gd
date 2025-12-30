f8daf332e56afcc2d74df4a9e388757cedd11078
thedevcube <111098996+thedevcube@users.noreply.github.com>
2025-12-30 07:17:35 -0300

    Update helper.gd

--- helper.gd
+++ helper.gd
@@ -5,3 +5,13 @@ func iterate_limiter(iterate_on: Variant, max_iterations_per_frame: int , lambda
 		icount += 1
 		lambda.call(iteration)
 		if icount >= max_iterations_per_frame: await ((get_tree().physics_frame) if use_physics_frame else (get_tree().process_frame))
+
+## Draw line uga uga
+func draw_line(from: Vector2 , to: Vector2 , _for: float , width: float = 5.0 , color: Color = Color.WHITE):
+	var line = Line2D.new()
+	get_tree().current_scene.add_child(line)
+	line.points = [line.to_local(from) , line.to_local(to)]
+	line.width = width
+	line.default_color = color
+	await get_tree().create_timer(_for).timeout
+	line.queue_free()

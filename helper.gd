a4f606cbbc95125e0d4cfbee6dcddab856910a65
thedevcube <111098996+thedevcube@users.noreply.github.com>
2026-01-31 18:09:51 -0300

    Update helper.gd

--- helper.gd
+++ helper.gd
@@ -15,3 +15,21 @@ func draw_line(from: Vector2 , to: Vector2 , _for: float , width: float = 5.0 , 
 	line.default_color = color
 	await get_tree().create_timer(_for).timeout
 	line.queue_free()
+
+## Returns all the planes on a meshinstance3D
+func _get_mesh_planes(mesh: MeshInstance3D) -> Array[Plane]:
+	var array: Array[Plane] = []
+
+	var face_id = -1
+	var face_array = []
+	const face_triangle_limit = 3
+
+	for face in mesh.mesh.get_faces():
+		face_id += 1
+		if face_id != face_triangle_limit:
+			face_array.append(face)
+		else:
+			array.append(Plane(face_array[0] , face_array[1] , face_array[2]))
+			face_array.clear(); face_id = -1
+
+	return array
